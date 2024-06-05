6bef65f61807f1c7ad18836254dc8e70272a1299
izuchukwu-eric <onukwubeizu@gmail.com>
2024-06-05 13:12:50 +0100

    add try catch

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { HumanloopStream } from '@/lib/humanloop-stream';
 import { StreamingTextResponse } from 'ai'
 import { Humanloop } from 'humanloop'
+import { NextRequest, NextResponse } from 'next/server';
 
 export const runtime = 'edge'
 
@@ -12,21 +13,35 @@ const humanloop = new Humanloop({
   apiKey: HUMANLOOP_API_KEY as string,
 })
 
-export async function POST(req: Request) {
+export const POST = async (req: NextRequest, res: NextResponse) => {
   if (!HUMANLOOP_API_KEY) {
     throw new Error('HUMANLOOP_API_KEY is not set')
   }
 
   const { messages } = await req.json()
 
-  const chatResponse = await humanloop.chatStream({
-    project: 'crypto-bot',
-    messages,
-    model_config: {
-      model: 'gpt-3.5-turbo',
-      temperature: 0.7
-    }
-  })
+  try {
+    const chatResponse = await humanloop.chatStream({
+      project: 'crypto-bot',
+      messages,
+      model_config: {
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7
+      }
+    })
 
-  return new StreamingTextResponse(HumanloopStream(chatResponse.data))
+    console.log("calling")
+    
+    return new StreamingTextResponse(HumanloopStream(chatResponse.data))
+  //   return new NextResponse(result, {
+  //     status: 200,
+  //     headers: { 'Content-Type': 'text/plain' },
+  // });
+  } catch (error) {
+    console.log("Error:", error); 
+    return new NextResponse(JSON.stringify({ error: "Failed to get completion from humanloop", details: error }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
